f75d79cd2386ba4c2f4f0120dd20cd99fa9664a9
AlejandroMoralesCernas <alejandromorales2003@hotmail.com>
2025-08-01 23:33:43 -0700

    Create Go Lang Air Functionality

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,18 @@ FROM golang:alpine
 #set working directory
 WORKDIR /app
 
+RUN apk add --no-cache git curl
+
+RUN go install github.com/air-verse/air@latest 
 #install necessary modules to compile golang
 COPY go.mod go.sum ./
 
-COPY ./cmd ./cmd
-
 RUN go mod download
 
-WORKDIR /app/cmd
+COPY . .
 
-RUN go build -o main .
+##RUN go build -o main ./cmd
 
 EXPOSE 8080
 
-CMD ["./main"]
+CMD ["air"]
--- cmd/main.go
+++ cmd/main.go
@@ -37,6 +37,7 @@ func main() {
 	// write a random print here
 	e.GET("/random", func(c echo.Context) error {
 		fmt.Println("This is a random print statement!")
+		fmt.Println("Generating a random value...")
 		// make a random variable and set it something
 		randomValue := 42
 		fmt.Println("Random value is:", randomValue)
@@ -75,4 +76,4 @@ func CalculationHandler(c echo.Context) error {
 		Outcome: result,
 		Message: "Calculation successful",
 	})
-}//return c.JSON(http.StatusOK, CalculationResponse{Outcome: result})
+}
