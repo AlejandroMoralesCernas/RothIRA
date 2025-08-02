4caf92a990a25bad43125c8480635be929b49048
Alexander Osorio <alexosorio1219@gmail.com>
2025-08-01 21:58:37 -0700

    add random-number endpoint for testing random values

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	// alex osorio added this import for random number generation
+	"math/rand"
 )
 
 type CalculationRequest struct {
@@ -41,6 +44,12 @@ func main() {
 		randomValue := 42
 		fmt.Println("Random value is:", randomValue)
 		return c.JSON(http.StatusOK, struct{ Message string }{Message: "Random print executed!"})
+	})
+
+	// random number generator
+	e.GET("/random-number", func(c echo.Context) error {
+		randomValue := rand.Intn(100) // Generate a random number between 0 and 99
+		return c.JSON(http.StatusOK, struct{ RandomNumber int }{RandomNumber: randomValue})
 	})
 
 	e.POST("/calculate", CalculationHandler)
